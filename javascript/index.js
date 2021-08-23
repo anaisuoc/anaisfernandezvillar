7900e0a9f016b7276b62befde5c1acf04f7e4778
anaisuoc <77071549+anaisuoc@users.noreply.github.com>
2021-08-23 13:20:32 +0200

    Add files via upload

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,4 @@
+
 /*menu responsive*/
 const hamburguer = document.querySelector('.menu-ham');
 const links = document.querySelector('ul');
@@ -17,3 +18,23 @@ box.forEach(function (popup) {
 			popup.classList.toggle('active');
 		});
 });
+
+
+let Ball = document.getElementsByClassName("ball");
+document.onmousemove = function() {
+
+  //get innerWidth,innerheight for browser
+  
+    //get horizontal coordinate of the onmousemove
+    let x = event.clientX * 100 / window.innerWidth + "%";
+
+    //get the verticle coordinate of onmousemove
+    let y = event.clientY * 100 / window.innerHeight + "%";
+
+    for(let i=0;i<2;i++) {
+        Ball[i].style.left = x;
+        Ball[i].style.top = y;
+        Ball[i].style.transform = "translate(-"+x+", -"+y+")";
+    }
+  
+}
